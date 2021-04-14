3a794efccb5fbe7af91bf5c3eb003896914ea4a2
DongHoBaek <ewdongho@gmail.com>
2021-04-15 00:55:55 +0900

    add database get

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ttt_project_003/drawer.dart';
 import 'package:ttt_project_003/posting_page.dart';
-import 'package:ttt_project_003/view_post_page.dart';
 import 'login.dart';
 
 class Home extends StatelessWidget {
+  final ref = FirebaseFirestore.instance.collection('posts');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,45 +18,59 @@ class Home extends StatelessWidget {
             return LoginWidget();
           } else {
             return Scaffold(
-              drawer: MyDrawer(snapshot),
-              appBar: AppBar(
-                backgroundColor: Colors.grey[600],
-                title: Text('Toward the Truth'),
-                actions: [
-                  TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    PostingPage()));
-                      },
-                      child: Text(
-                        '게시물 작성',
-                        style: TextStyle(color: Colors.white),
-                      ))
-                ],
-              ),
-              body: ListView.builder(
-                padding: EdgeInsets.only(bottom: 10),
-                itemCount: 10,
-                itemBuilder: (context, index){
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.grey[300],
-                    ),
-                    margin: EdgeInsets.only(top: 10, right: 5, left: 5),
-                    height: 100,
-                    child: ListTile(
-                      onTap: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => ViewPostPage()));
-                      },
-                    ),
-                  );
-                },
-              ),
-            );
+                drawer: MyDrawer(snapshot),
+                appBar: AppBar(
+                  backgroundColor: Colors.grey[600],
+                  title: Text('Toward the Truth'),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      PostingPage()));
+                        },
+                        child: Text(
+                          '게시물 작성',
+                          style: TextStyle(color: Colors.white),
+                        ))
+                  ],
+                ),
+                body: StreamBuilder<QuerySnapshot>(
+                  stream: ref.snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Something went wrong');
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Text("Loading");
+                    }
+
+                    return new ListView(
+                      children:
+                          snapshot.data.docs.map((DocumentSnapshot document) {
+                        return Container(
+                          height: 100,
+                          margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.grey[400],
+                          ),
+                          child: new ListTile(
+                            onTap: (){
+
+                            },
+                            title: new Text(document.data()['title']),
+                            subtitle: new Text(document.data()['content']),
+                          ),
+                        );
+                      }).toList(),
+                    );
+                  },
+                ));
           }
         },
       ),
--- lib/posting_page.dart
+++ lib/posting_page.dart
@@ -2,52 +2,80 @@ import 'package:flutter/material.dart';
 import 'models/post.dart';
 
 class PostingPage extends StatelessWidget {
-
   TextEditingController titleController = TextEditingController();
   TextEditingController contentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Posting Page'),
-        actions: [
-          TextButton(onPressed: (){
-            AddPost post = AddPost(titleController.text, contentController.text);
-            post.addPost();
-            Navigator.pop(context);
-          }, child: Text('save', style: TextStyle(color: Colors.white, fontSize: 20),))
-        ],
-      ),
       body: Container(
         padding: EdgeInsets.all(20),
         child: Column(
           children: [
+            SizedBox(
+              height: 10,
+            ),
             Row(
               children: [
                 IconButton(
-                  icon: Icon(Icons.arrow_back_ios_outlined),onPressed: (){},
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
                 Flexible(
                   child: TextFormField(
                     controller: titleController,
                     decoration: InputDecoration(
+                      border: InputBorder.none,
                       hintText: 'title',
                     ),
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 10,),
+            Divider(
+              indent: 17,
+              endIndent: 17,
+              color: Colors.black,
+            ),
             Expanded(
-              child: TextFormField(
-                controller: contentController,
-                expands: true,
-                maxLines: null,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'content',
+              child: Container(
+                padding: EdgeInsets.all(10),
+                width: 340,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black38),
                 ),
+                child: TextFormField(
+                  controller: contentController,
+                  expands: true,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'content',
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 80,
+              height: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(color: Colors.black38)),
+              child: IconButton(
+                onPressed: () {
+                  AddPost post = AddPost(titleController.text, contentController.text);
+
+                  post.addPost();
+
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.save_outlined),
               ),
             ),
           ],
