8e4f3ec6a1af9c55cfe21f5e432bc5675291b186
DongHoBaek <ewdongho@gmail.com>
2021-05-08 22:33:38 +0900

    - UI complete not yet(70/100)

--- lib/UI/user_home_page.dart
+++ lib/UI/user_home_page.dart
@@ -11,7 +11,20 @@ class UserHomePage extends Page {
 
 class UserHome extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+    Size size  = MediaQuery.of(context).size;
+
+    Widget _buildPostButton(Function onTap) {
+      return Container(
+        margin: EdgeInsets.all(10),
+        height: size.height * 0.3,
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+        ),
+        child: ListTile(onTap: onTap),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
           iconTheme: IconThemeData(color: Colors.black),
@@ -21,63 +34,12 @@ class UserHome extends StatelessWidget {
             'MyPage',
             style: TextStyle(color: Colors.black),
           )),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return _buildPostButton(() {});
+        }
+        ),
     );
   }
 }
-
-// class MyPage extends StatelessWidget {
-//   final ref = FirebaseFirestore.instance.collection('posts');
-//   final String uid;
-//
-//   MyPage(this.uid);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: StreamBuilder<QuerySnapshot>(
-//             stream: ref.snapshots(),
-//             builder:
-//                 (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//               if (snapshot.hasError) {
-//                 return Center(child: Text('Something went wrong'));
-//               }
-//
-//               if (snapshot.connectionState == ConnectionState.waiting) {
-//                 return Center(child: CircularProgressIndicator());
-//               }
-//
-//               return Container(
-//                 color: Colors.white,
-//                 child: new ListView(
-//                   children: snapshot.data.docs.map((DocumentSnapshot document) {
-//                     if (document.data()['uid'] == uid) {
-//                       return Container(
-//                         height: 100,
-//                         margin: EdgeInsets.only(top: 10, left: 10, right: 10),
-//                         decoration: BoxDecoration(
-//                           borderRadius: BorderRadius.circular(10),
-//                           color: Colors.grey[200],
-//                         ),
-//                         child: new ListTile(
-//                           onTap: () {
-//                             Navigator.push(
-//                                 context,
-//                                 MaterialPageRoute(
-//                                     builder: (context) =>
-//                                         ViewPostPage(uid, document.data()['unm'], docToView: document)))
-//                                 .then((value)=>Provider.of<CurrentDocId>(context, listen: false).setCurrentDocId(null));
-//                           },
-//                           title: new Text(
-//                               '${document.data()['unm']}\n${document.data()['title']}', overflow: TextOverflow.ellipsis),
-//                           subtitle: new Text(document.data()['content'], overflow: TextOverflow.ellipsis),
-//                         ),
-//                       );
-//                     }else{
-//                       return Container();
-//                     }
-//                   }).toList(),
-//                 ),
-//               );
-//             }));
-//   }
-// }
