2ddeaa8f8fda5d0556f916a4029ed5ce18845921
unknown <tjsan1998@mail.ulsan.ac.kr>
2021-06-29 19:34:11 +0900

    delete import auth in post provider

--- lib/models/post_provider.dart
+++ lib/models/post_provider.dart
@@ -1,9 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PostProvider with ChangeNotifier {
-  User user = FirebaseAuth.instance.currentUser;
 
   String _currentDocId;
   String _rootPostDID;
