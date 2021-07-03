86b5b3afb4df2e719f280311e7bff3c03ef4c956
unknown <tjsan1998@mail.ulsan.ac.kr>
2021-07-03 17:45:56 +0900

    merge with master

--- lib/models/user_provider.dart
+++ lib/models/user_provider.dart
@@ -8,9 +8,9 @@ class UserProvider extends ChangeNotifier {
   String _email;
   String _nickname;
   String _profileMessage;
-  List<String> _myPosts;
-  List<String> _bookmarkedPosts;
-  List<String> _likedPosts;
+  static List _myPosts;
+  static List _bookmarkedPosts = [];
+  static List _likedPosts;
   String _profileImage;
 
   String get uid => _uid;
@@ -23,11 +23,11 @@ class UserProvider extends ChangeNotifier {
 
   String get profileMessage => _profileMessage;
 
-  List<String> get myPosts => _myPosts;
+  List get myPosts => _myPosts;
 
-  List<String> get bookmarkedPosts => _bookmarkedPosts;
+  List get bookmarkedPosts => _bookmarkedPosts;
 
-  List<String> get likedPosts => _likedPosts;
+  List get likedPosts => _likedPosts;
 
   String get profileImage => _profileImage;
 
@@ -38,11 +38,9 @@ class UserProvider extends ChangeNotifier {
 
     _uid = user.uid;
 
-    await users
-        .doc(_uid)
-        .get()
-        .then((DocumentSnapshot documentSnapshot) {
-      Map<String, dynamic> data = documentSnapshot.data() as Map<String, dynamic>;
+    await users.doc(_uid).get().then((DocumentSnapshot documentSnapshot) {
+      Map<String, dynamic> data =
+          documentSnapshot.data() as Map<String, dynamic>;
       if (documentSnapshot.exists) {
         _email = data['email'];
         _username = data['username'];
@@ -51,7 +49,8 @@ class UserProvider extends ChangeNotifier {
         _bookmarkedPosts = data['bookmarked_posts'];
         _likedPosts = data['liked_posts'];
         _profileImage = data['profile_image'];
-        print('get data!');
+        print('get user data!');
+        print(_bookmarkedPosts);
 
         notifyListeners();
       } else {
@@ -61,7 +60,7 @@ class UserProvider extends ChangeNotifier {
     });
   }
 
-  void logout(){
+  void logout() {
     _uid = null;
     _email = null;
     _username = null;
@@ -75,122 +74,110 @@ class UserProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void register(User user){
+  void register(User user) {
     DocumentReference ref = users.doc(_uid);
 
-    ref.set({
-      'email':user.email,
-      'username':user.displayName,
-      'nickname':null,
-      'profileMessage':null,
-      'my_posts':[],
-      'bookmarked_posts':[],
-      'liked_posts':[],
-      'profile_image':""
+    ref
+        .set({
+          'email': user.email,
+          'username': user.displayName,
+          'nickname': null,
+          'profileMessage': null,
+          'my_posts': [],
+          'bookmarked_posts': [],
+          'liked_posts': [],
+          'profile_image': ""
         })
         .then((value) => print("User Registed"))
         .catchError((error) => print("Failed to regist user: $error"));
     login();
   }
 
-  void degister(){
-    users.doc(_uid).delete()
+  void degister() {
+    users
+        .doc(_uid)
+        .delete()
         .then((value) => print("User Degist"))
         .catchError((error) => print("Failed to Degist user: $error"));
   }
 
-  bool updateProfile(String nickname, String profileMessage, String imageUrl){
-    users.doc(_uid)
-        .update({
-          'nickname': nickname,
-          'profileMessage': profileMessage,
-          'profile_image': imageUrl
-        })
-        .then((value) {
-          print("Profile updated");
-          _nickname = nickname;
-          _profileMessage = profileMessage;
-          _profileImage = profileImage;
-        })
-        .catchError((error) => print("Failed to update profile: $error"));
+  bool updateProfile(String nickname, String profileMessage, String imageUrl) {
+    users.doc(_uid).update({
+      'nickname': nickname,
+      'profileMessage': profileMessage,
+      'profile_image': imageUrl
+    }).then((value) {
+      print("Profile updated");
+      _nickname = nickname;
+      _profileMessage = profileMessage;
+      _profileImage = profileImage;
+    }).catchError((error) => print("Failed to update profile: $error"));
     notifyListeners();
   }
 
   void bookmark(String postDid) {
-    _bookmarkedPosts.add(postDid);
-    _bookmarkedPosts.sort();
-    users.doc(_uid)
-        .update({
-          'bookmarked_posts': _bookmarkedPosts
-        })
-        .then((value) {
-          print("bookmarked");
-        })
-        .catchError((error) => print("Failed to bookmark: $error"));
+    print(postDid);
+    _bookmarkedPosts.insert(0, postDid);
+    print(_bookmarkedPosts);
+
+    users
+        .doc(_uid)
+        .update({'bookmarked_posts': _bookmarkedPosts}).then((value) {
+      print("bookmarked");
+    }).catchError((error) => print("Failed to bookmark: $error"));
     notifyListeners();
   }
 
-  void unbookmark(String postDid){
+  void unbookmark(String postDid) {
+    print(postDid);
     _bookmarkedPosts.remove(postDid);
-    users.doc(_uid)
-        .update({
-          'bookmarked_posts': _bookmarkedPosts
-        })
-        .then((value) {
-          print("unbookmark");
-        })
-        .catchError((error) => print("Failed to unbookmark: $error"));
+    print(_bookmarkedPosts);
+    users
+        .doc(_uid)
+        .update({'bookmarked_posts': _bookmarkedPosts}).then((value) {
+      print("unbookmark");
+    }).catchError((error) => print("Failed to unbookmark: $error"));
     notifyListeners();
   }
 
-  bool isBookmarked(String postDid){
-    bool result;
-
-    if(_bookmarkedPosts == null){
-      result = false;
-    } else {
-      result = _bookmarkedPosts.contains(postDid);
-    }
-
-    return result;
+  bool isBookmarked(String postDid) {
+    bool result = false;
+      if(_bookmarkedPosts != null) {
+        result = _bookmarkedPosts.contains(postDid);
+      }
+      return result;
   }
 
   void like(String postDid) {
-    _likedPosts.add(postDid);
-    _likedPosts.sort();
-    users.doc(_uid)
-        .update({
-      'liked_posts': _likedPosts
-    })
-        .then((value) {
+    print(postDid);
+    _likedPosts.insert(0, postDid);
+    print(_likedPosts);
+
+    users
+        .doc(_uid)
+        .update({'liked_posts': _likedPosts}).then((value) {
       print("liked");
-    })
-        .catchError((error) => print("Failed to like: $error"));
+    }).catchError((error) => print("Failed to like: $error"));
     notifyListeners();
   }
 
-  void unlike(String postDid){
+  void unlike(String postDid) {
+    print(postDid);
     _likedPosts.remove(postDid);
-    users.doc(_uid)
-        .update({
-      'liked_posts': _likedPosts
-    })
-        .then((value) {
+    print(_likedPosts);
+    users
+        .doc(_uid)
+        .update({'liked_posts': _likedPosts}).then((value) {
       print("unlike");
-    })
-        .catchError((error) => print("Failed to unlike: $error"));
+    }).catchError((error) => print("Failed to unlike: $error"));
     notifyListeners();
   }
 
-  bool isLiked(String postDid){
-    bool result;
-
-    if(_likedPosts == null){
-      result = false;
-    } else {
+  bool isLiked(String postDid) {
+    bool result = false;
+    if(_likedPosts != null) {
       result = _likedPosts.contains(postDid);
     }
-
     return result;
   }
 }
